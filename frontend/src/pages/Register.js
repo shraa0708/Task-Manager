f9118e7aca41638bfb61b2be52357111e9cd645c
shraa0708 <84589827+shraa0708@users.noreply.github.com>
2025-03-24 13:39:56 +0530

    Update Register.js

--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -27,7 +27,7 @@ const Register = () => {
         }
 
         try {
-            const response = await axios.post('http://localhost:5000/api/auth/register', formData, {
+            const response = await axios.post('https://task-manager-zetw.onrender.com/api/auth/register', formData, {
                 withCredentials: true,
             });
 
