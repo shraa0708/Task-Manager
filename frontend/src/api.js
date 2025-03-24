fc197905e2a19a42b12f903f522fa369d6861b0b
shraa0708 <84589827+shraa0708@users.noreply.github.com>
2025-03-24 13:38:01 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: 'https://task-manager-zetw.onrender.com/api',
     withCredentials: true,
 });
 axios.defaults.withCredentials = true;
-axios.get('http://localhost:5000/api/tasks', { withCredentials: true });
+axios.get('https://task-manager-zetw.onrender.com/api/tasks', { withCredentials: true });
 
 
 export default api;
