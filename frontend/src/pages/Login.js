f281b2810b8fbd1ef0c93c44cfbd3608f429d3a4
shraa0708 <84589827+shraa0708@users.noreply.github.com>
2025-03-24 13:39:32 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -24,7 +24,7 @@ const Login = () => {
         }
 
         try {
-            const res = await axios.post('http://localhost:5000/api/auth/login', formData, {
+            const res = await axios.post('https://task-manager-zetw.onrender.com/api/auth/login', formData, {
                 withCredentials: true,
             });
 
